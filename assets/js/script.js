7c39e0a3018eeffaa8b7340bbab406859d37ba2e
linx02 <elvius.linus@gmail.com>
2023-07-06 12:13:50 +0200

    Add rungame function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,19 @@
-function runGame(){
+let whoPlays = Math.random() < 0.5 ? 'x' : 'o';
 
+function runGame(){
+    let grid = document.getElementsByClassName('cell');
+    document.getElementById('game-heading').innerHTML = `${whoPlays.toUpperCase()} starts!`
+
+    for(let cell of grid){
+        cell.addEventListener('click', function(){
+            placePawn(cell);
+        });
+    }
 }
 
-function placePawn(){
-
+function placePawn(cell){
+    cell.innerHTML = whoPlays;
+    whoPlays = whoPlays === 'x' ? 'o' : 'x';
 }
 
 function checkWin(){
@@ -13,3 +23,5 @@ function checkWin(){
 function incrementScore(){
     
 }
+
+runGame();
