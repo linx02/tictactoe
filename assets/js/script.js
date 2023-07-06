51c9d13c3512c9971ee3138dec7b45e617cbcb34
linx02 <elvius.linus@gmail.com>
2023-07-06 12:59:22 +0200

    Add checkWin function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 let whoPlays = Math.random() < 0.5 ? 'x' : 'o';
+let occupied = {};
 
 function runGame(){
     let grid = document.getElementsByClassName('cell');
@@ -17,12 +18,52 @@ function placePawn(cell){
         return;
     }
     cell.innerHTML = whoPlays;
+
+    occupied[cell.id] = whoPlays;
+
     whoPlays = whoPlays === 'x' ? 'o' : 'x';
 
     checkWin();
 }
 
 function checkWin(){
+    let winCombos = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+        [1, 4, 7],
+        [2, 5, 8],
+        [3, 6, 9],
+        [1, 5, 9],
+        [3, 5, 7]
+    ];
+    
+    let x = [];
+    let o = [];
+
+    for (let key in occupied){
+        if (occupied[key] === 'x') {
+            x.push(parseInt(key[key.length - 1]));
+        }
+        else if (occupied[key] === 'o') {
+            o.push(parseInt(key[key.length - 1]));
+        }
+    }
+
+    console.log(`X has : ${x} and O has : ${o}`);
+
+    for (let combo of winCombos){
+        let result = combo.every(num => x.includes(num));
+        if (result === true) {
+            alert('X won');
+        }
+    }
+    for (let combo of winCombos){
+        let result = combo.every(num => o.includes(num));
+        if (result === true) {
+            alert('O won');
+        }
+    }
 }
 
 function incrementScore(){
